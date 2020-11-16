faa9642771f371e44109a2b82ee7a1e1ae7f7c19
Marcel Ullrich <ullrich.marcel@web.de>
2020-11-16 22:23:08 +0100

    new markdown url

--- commands/markdown.js
+++ commands/markdown.js
@@ -7,7 +7,7 @@ module.exports = {
 	async execute(message,args) {
 
     filePath="template.md";
-    shell.exec("curl -v -XPOST -H 'Content-Type: text/markdown' 'https://demo.codimd.org/new' --data-binary @"+filePath+" > /tmp/Url.txt")
+    shell.exec("curl -v -XPOST -H 'Content-Type: text/markdown' 'https://demo.hedgedoc.org/new' --data-binary @"+filePath+" > /tmp/Url.txt")
     url=fs.readFileSync("/tmp/Url.txt","utf8").replace("Found. Redirecting to ","")+"?both";
     message.channel.send("Your markdown is "+url);
 
--- template.md
+++ template.md
@@ -1,6 +1,14 @@
 $$
 \newcommand{\nat}{\mathbb{N}}
 \renewcommand{\phi}{\varphi}
+\newcommand{\N}{\mathbb{N}}
+\newcommand{\NN}{\mathbb{N}}
+\newcommand{\Z}{\mathbb{N}}
+\newcommand{\ZZ}{\mathbb{N}}
+\newcommand{\O}{\mathcal{O}}
+\newcommand{\OO}{\mathcal{O}}
+\newcommand{\T}{\Theta}
+\newcommand{\TT}{\Theta}
 $$
 
 <!-- Features: https://demo.codimd.org/features?both -->
